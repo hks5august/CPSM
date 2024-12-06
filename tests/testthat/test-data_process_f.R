e05b5ddaf51b794c6ad64f04659631eb9469b91e
Harpreet kaur <46756503+hks5august@users.noreply.github.com>
2024-12-06 07:37:21 +0530

    Add files via upload

--- tests/testthat/test-data_process_f.R
+++ tests/testthat/test-data_process_f.R
@@ -1,5 +1,8 @@
 test_that("Assess_data_process", {
   data(Example_TCGA_LGG_FPKM_data, package = "CPSM")
-  New_data <- data_process_f(Example_TCGA_LGG_FPKM_data, col_num=20, surv_time = "OS.time")
+  New_data <- data_process_f(assays(Example_TCGA_LGG_FPKM_data)$expression,
+                             col_num=20, surv_time = "OS.time")
   expect_s3_class(New_data, "data.frame")
   })
+
+
