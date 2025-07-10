cb65e3c5973534c958224bf03a2e3009f8e73ce6
Harpreet Kaur <hks04180@gmail.com>
2025-07-10 15:26:09 -0400

    Update km_overlay_plot_f.R function to improve figure

--- R/km_overlay_plot_f.R
+++ R/km_overlay_plot_f.R
@@ -167,20 +167,11 @@ km_overlay_plot_f <- function(Train_results, Test_results, survcurve_te_data, se
   selected_test_pred_Prob <- Test_results$Prediction_Prob[rownames(Test_results) == selected_sample]
 
   # Create survival object for the test sample
-  test_sample$event <- ifelse(test_sample$Survival_Probability < 1, 1, 0)
-  test_sample_surv <- survival::Surv(time = test_sample$time_point, event = test_sample$event)
-  km_test_fit <- survfit(test_sample_surv ~ 1)
-
-  # Prepare survival curve data
+ # Create test sample survival curve
   test_curve <- data.frame(
-    time = km_test_fit$time,
-    surv = km_test_fit$surv
+    time = test_sample$time_point,
+    surv = test_sample$Survival_Probability
   )
-
-  # Calculate median survival time for test sample
-  median_test_surv_time <- tryCatch({
-    test_curve$time[which.min(abs(test_curve$surv - 0.5))]
-  }, error = function(e) { NA })
 
   # Add test sample curve and median line to KM plot
   km_Train_results_plot2 <- km_Train_results_plot$plot +
@@ -194,14 +185,16 @@ km_overlay_plot_f <- function(Train_results, Test_results, survcurve_te_data, se
 
     annotate(
       "text",
-      x = max(test_curve$time) * 1.5,
-      y = 0.8,
+      x = max(Train_results$OS_month) * 0.7,
+      y = 0.85,
       label = paste("Sample:", selected_sample,
                     "\nPredicted Risk:", selected_test_pred_risk,
                     "\nPrediction Probability:", round(selected_test_pred_Prob, 3)),
       color = "darkgreen",
-      fontface = "bold"
+      fontface = "bold",
+      hjust = 0
     )
+
 
   return(km_Train_results_plot2)
 }
