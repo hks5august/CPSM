84d3bcd12bc65c138abbdb317afb3f689e7156a0
Harpreet Kaur <hks04180@gmail.com>
2025-09-04 15:15:40 -0400

    Update MTLR_pred_model_f function to calculate IBS

--- R/MTLR_pred_model_f.R
+++ R/MTLR_pred_model_f.R
@@ -204,53 +204,23 @@ MTLR_pred_model_f <- function(train_clin_data, test_clin_data, Model_type,
     mean_mae_te <- round(mean(abs(survival_summary_te$OS_month - survival_summary_te$Mean), na.rm = TRUE), 2)
     median_mae_te <- round(median(abs(survival_summary_te$OS_month - survival_summary_te$Median), na.rm = TRUE), 2)
     
-    #Error_mat_tr <- cbind(c_index1_tr,  mean_mae_tr,  median_mae_tr)
-    #Error_mat_te <- cbind(c_index1_te,  mean_mae_te,  median_mae_te)
+    Error_mat_tr <- cbind(c_index1_tr,  mean_mae_tr,  median_mae_tr)
+    Error_mat_te <- cbind(c_index1_te,  mean_mae_te,  median_mae_te)
     
-    #Error_mat <- rbind(Error_mat_tr , Error_mat_te)
-    #colnames(Error_mat) <- c("C_index", "Mean_MAE", "Median_MAE")
-    #rownames(Error_mat) <- c("Training_set", "Test_set")
+    Error_mat <- rbind(Error_mat_tr , Error_mat_te)
+    colnames(Error_mat) <- c("C_index", "Mean_MAE", "Median_MAE")
+    rownames(Error_mat) <- c("Training_set", "Test_set")
    
     # IBS calculation
     # Training data
     # Survival probabilities at event times
     
-    survivalProbs_p1_tr <- predict(Mod1, sel_clin_tr2, type = "prob_times")
-   
-    #extract prob times at diff times points
-    survivalProbs_t_mat_tr <-as.matrix(survivalProbs_p1_tr )
-    survivalProbs_t_mat1_tr <- survivalProbs_t_mat_tr[,-1]
-    survivalProbs_t_mat1_t_tr <- t(survivalProbs_t_mat1_tr )
-    survivalProbs_t_mat1_t2_tr <- survivalProbs_t_mat1_t_tr[,-1]
-   
-   # Matrix of survival probabilities (remove time column, then transpose if needed)
-   #sp_matrix_tr <- t(as.matrix(survivalProbs_p1_tr[ , -1]))[ , -1]
 
-   # Integrated Brier Score (IBS)
-   ibs_tr <- round(pec::IBS(surv_obj1_tr, sp_matrix = survivalProbs_t_mat1_t2_tr,
-      times = survivalProbs_p1_tr$time[-1]),3)
+    #Error_mat_tr <- cbind(c_index1_tr, mean_mae_tr, median_mae_tr, round(ibs_tr, 3))
+    #Error_mat_te <- cbind(c_index1_te, mean_mae_te, median_mae_te, round(ibs_te, 3))
 
-   # Test data
-    # Survival probabilities at event times
-     #Test data
-    survivalProbs_p1 <- predict(Mod1, sel_clin_te2, type = "prob_times")
-    #extract prob times at diff times points
-    survivalProbs_t_mat <-as.matrix(survivalProbs_p1 )
-    survivalProbs_t_mat1 <- survivalProbs_t_mat[,-1]
-    survivalProbs_t_mat1_t <- t(survivalProbs_t_mat1 )
-    survivalProbs_t_mat1_t2 <- survivalProbs_t_mat1_t[,-1]
-   # Matrix of survival probabilities (remove time column, then transpose if needed)
-   #sp_matrix_te <- t(as.matrix(survivalProbs_p1_te[ , -1]))[ , -1]
-
-   # Integrated Brier Score (IBS)
-   ibs_te <- round(pec::IBS(surv_obj1_te, sp_matrix = survivalProbs_t_mat1_t2,
-                       survivalProbs_p1$time[-1]),3)   
-
-    Error_mat_tr <- cbind(c_index1_tr, mean_mae_tr, median_mae_tr, round(ibs_tr, 3))
-    Error_mat_te <- cbind(c_index1_te, mean_mae_te, median_mae_te, round(ibs_te, 3))
-
-    colnames(Error_mat_tr) <- colnames(Error_mat_te) <- c("C_index", "Mean_MAE", "Median_MAE", "IBS")
-    rownames(Error_mat) <- c("Training_set", "Test_set")
+    #colnames(Error_mat_tr) <- colnames(Error_mat_te) <- c("C_index", "Mean_MAE", "Median_MAE", "IBS")
+    #rownames(Error_mat) <- c("Training_set", "Test_set")
 
  }
 
