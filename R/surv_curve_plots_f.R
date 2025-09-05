d024685435f47dd74e95704cf47286b5c3de06b6
Harpreet Kaur <hks04180@gmail.com>
2025-09-05 16:50:01 -0400

    surv_curve_plots_f.R and test-surv_curve_plots_f.R  to get cutomized font size and color of the plot

--- R/surv_curve_plots_f.R
+++ R/surv_curve_plots_f.R
@@ -8,14 +8,6 @@
 #' other columns corresponding to patient survival probabilities.
 #' @param selected_sample A character string specifying the patient ID to be
 #' highlighted in the plot.
-#' @param font_size Numeric. Font size for axis labels, titles, and legends.
-#'   Must be supplied by the user.
-#' @param line_size Numeric. Line thickness for plotted curves. Must be supplied
-#'   by the user.
-#' @param all_line_col Character. Color used for all non-highlighted patients
-#'   (e.g., `"black"`). Must be supplied by the user.
-#' @param highlight_col Character. Color used to highlight the selected patient
-#'   (e.g., `"red"`). Must be supplied by the user.
 #'
 #' @return A list containing two ggplot2 objects:
 #'   \item{all_patients_plot}{A ggplot object displaying survival curves for
@@ -38,22 +30,22 @@
 #' @name surv_curve_plots_f
 #'
 #' @examples
+#' # Example survival curve data
 #' Surv_curve_data <- data.frame(
 #'   time_point = c(0, 1, 2, 3, 4),
 #'   Patient1 = c(1, 0.9, 0.8, 0.7, 0.6),
 #'   Patient2 = c(1, 0.85, 0.75, 0.65, 0.55)
 #' )
 #'
-#' plots <- surv_curve_plots_f(
-#'   Surv_curve_data,
-#'   selected_sample = "Patient1",
-#'   font_size = 12,
-#'   line_size = 0.5,
-#'   all_line_col = "black",
-#'   highlight_col = "red"
-#' )
+#' # Generate plots with Patient1 highlighted
+#' plots <- surv_curve_plots_f(Surv_curve_data, selected_sample = "Patient1")
+#'
+#' # View the plots
 #' print(plots$all_patients_plot)
 #' print(plots$highlighted_patient_plot)
+#'
+#' @import ggplot2
+#' @import reshape2
 #'
 #' @export
 
@@ -61,9 +53,7 @@
 utils::globalVariables(c("Time", "Value", "Patient"))
 
 
-
-surv_curve_plots_f <- function(Surv_curve_data, selected_sample, font_size,  
-				line_size , all_line_col, highlight_col){
+surv_curve_plots_f <- function(Surv_curve_data, selected_sample) {
   # load data
   survCurves_data <- Surv_curve_data
 
@@ -73,22 +63,21 @@ surv_curve_plots_f <- function(Surv_curve_data, selected_sample, font_size,
   # add column names
   colnames(survCurves_m) <- c("Time", "Patient", "Value")
 
-  #Plot 1: all patients #
   # create survival curve plot for all samples
   Surv_curv_plot_all_pat <- ggplot(survCurves_m, aes(
     x = Time, y = Value,
     group = Patient,
     color = Patient
   )) +
-    geom_line(size = line_size) +
+    geom_line() +
     labs(x = "Time in Months", y = "Survival Probability") +
     ggtitle("Survival Curves for Patients") +
     geom_hline(yintercept = 0.5, color = "black", linetype = "dashed") +
     # add dashed line corresonds to 0.5 probability
     theme(
       legend.position = "bottom", legend.box = "vertical",
-      legend.title = element_text(size = font_size), legend.key = element_blank(),
-      legend.key.size = unit(3, "mm"), legend.text = element_text(size = font_size)
+      legend.title = element_text(), legend.key = element_blank(),
+      legend.key.size = unit(3, "mm"), legend.text = element_text(size = 4)
     ) +
     guides(color = guide_legend(title = "Patients"))
 
@@ -105,28 +94,43 @@ surv_curve_plots_f <- function(Surv_curve_data, selected_sample, font_size,
     message("The sample", selected_sample, "is present in the Test dataset.\n")
   }
 
-
-  # Plot 2: highlight selected patient #
-  Surv_curv_plot_highlight <- ggplot(survCurves_m, aes(x = Time, y = Value)) +
-    geom_line(data = subset(survCurves_m, Patient != selected_sample),
-              aes(group = Patient),
-              color = all_line_col, size = line_size) +
-    geom_line(data = subset(survCurves_m, Patient == selected_sample),
-              aes(group = Patient, color = "Selected"),
-              size = line_size) +
-    scale_color_manual(values = c("Selected" = highlight_col)) +
-    labs(x = "Time in Months", y = "Survival Probability", color = "Highlighted") +
-    ggtitle("Survival Curves with Highlighted Patient") +
+  # create survival curve plot for selected patient
+  Surv_curv_plot_all_pats_with_highlighting_one_pat <- ggplot(
+    survCurves_m, aes(
+      x = Time, y = Value, linetype = Patient,
+      color = Selected_patient
+    )
+  ) +
+    scale_y_continuous(breaks = seq(0, 1, 0.1)) +
+    ggtitle("Survival Curves for Patients with Highlightited Patient") +
     geom_hline(yintercept = 0.5, color = "red", linetype = "dashed") +
+    # Set linetype for all lines based on Patient
+    geom_line(size = 0.5) +
+    # Highlight one patient with a specific color
+    scale_linetype_manual(values = rep(
+      "solid",
+      length(unique(survCurves_m$Patient))
+    )) +
+    scale_color_manual(values = c("black", "red")) +
+    geom_line(aes(colour = "yellow"),
+      size = 0.5,
+      data = ~ subset(
+        survCurves_m,
+        Patient == Selected_patient
+      )
+    ) +
+    labs(x = "Time in Months", y = "Survival Probability") +
     theme(
       legend.position = "bottom",
-      legend.title = element_text(size = font_size),
-      legend.text = element_text(size = font_size),
-      axis.title = element_text(size = font_size),
-      axis.text = element_text(size = font_size),
-      plot.title = element_text(size = font_size, face = "bold")
-    )  
- 
+      legend.title = element_text(),
+      legend.key = element_blank(),
+      legend.key.size = unit(3, "mm"),
+      legend.text = element_text(size = 4)
+    ) +
+    guides(
+      linetype = guide_legend(title = "Patients"),
+      color = guide_legend(title = "Selected Patient")
+    )
 
   # Return the plots as a list
   return(list(
--- tests/testthat/test-surv_curve_plots_f.R
+++ tests/testthat/test-surv_curve_plots_f.R
@@ -1,16 +1,7 @@
 test_that("multiplication works", {
   data(survCurves_data, package = "CPSM")
-
-# Call the function
-  p <- surv_curve_plots_f(
-    Surv_curve_data = Surv_curve_data,
-    selected_sample = "TCGA_TQ_A8XE_01",
-    font_size = 12,
-    line_size = 0.5,
-    all_line_col = "black",
-    highlight_col = "red"
-  )
-
+  p <- surv_curve_plots_f(Surv_curve_data = survCurves_data,
+                          selected_sample = "TCGA-TQ-A8XE-01")
   # Check that the output is a list
   expect_type(p, "list")
 
