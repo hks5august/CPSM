1b8e95dc0bbbbcab708840592c06c8f317a862f5
Harpreet Kaur <hks04180@gmail.com>
2025-09-05 23:54:22 -0400

    surv_curve_plots_f.R vignettes/CPSM.Rmd  and tests/testthat/test-surv_curve_plots_f.R  to get cutomized font size and color of the plot

--- tests/testthat/test-surv_curve_plots_f.R
+++ tests/testthat/test-surv_curve_plots_f.R
@@ -1,7 +1,8 @@
-test_that("multiplication works", {
+test_that("surv_curve_plots_f works with defaults", {
   data(survCurves_data, package = "CPSM")
   p <- surv_curve_plots_f(Surv_curve_data = survCurves_data,
                           selected_sample = "TCGA-TQ-A8XE-01")
+
   # Check that the output is a list
   expect_type(p, "list")
 
@@ -10,5 +11,26 @@ test_that("multiplication works", {
   expect_s3_class(p$highlighted_patient_plot, "ggplot")
 })
 
+test_that("surv_curve_plots_f applies custom styling arguments", {
+  data(survCurves_data, package = "CPSM")
+  p <- surv_curve_plots_f(
+    Surv_curve_data = survCurves_data,
+    selected_sample = "TCGA-TQ-A8XE-01",
+    font_size = 16,
+    line_size = 2,
+    all_line_col = "blue",
+    highlight_col = "orange"
+  )
+
+  # Still should return list of ggplot objects
+  expect_type(p, "list")
+  expect_s3_class(p$all_patients_plot, "ggplot")
+  expect_s3_class(p$highlighted_patient_plot, "ggplot")
+
+  # Check that custom color is applied to highlight
+  # (pull data and check mapping)
+  highlighted_layer <- p$highlighted_patient_plot$layers[[2]]
+  expect_true("orange" %in% unlist(highlighted_layer$aes_params))
+})
 
 
