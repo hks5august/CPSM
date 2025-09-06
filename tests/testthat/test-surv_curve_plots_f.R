e76a145dfa4d0650e5a2fbf082ac073a60c36ec7
Harpreet Kaur <hks04180@gmail.com>
2025-09-06 00:05:29 -0400

    tests/testthat/test-surv_curve_plots_f.R  to get cutomized font size and color of the plot

--- tests/testthat/test-surv_curve_plots_f.R
+++ tests/testthat/test-surv_curve_plots_f.R
@@ -16,8 +16,8 @@ test_that("surv_curve_plots_f applies custom styling arguments", {
   p <- surv_curve_plots_f(
     Surv_curve_data = survCurves_data,
     selected_sample = "TCGA-TQ-A8XE-01",
-    font_size = 16,
-    line_size = 2,
+    font_size = 10,
+    line_size = 0.5,
     all_line_col = "blue",
     highlight_col = "orange"
   )
@@ -27,10 +27,5 @@ test_that("surv_curve_plots_f applies custom styling arguments", {
   expect_s3_class(p$all_patients_plot, "ggplot")
   expect_s3_class(p$highlighted_patient_plot, "ggplot")
 
-  # Check that custom color is applied to highlight
-  # (pull data and check mapping)
-  highlighted_layer <- p$highlighted_patient_plot$layers[[2]]
-  expect_true("orange" %in% unlist(highlighted_layer$aes_params))
 })
-
 
