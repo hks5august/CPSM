7be3b894983a9646497825f58af59d62ca8ea64e
Harpreet Kaur <hks04180@gmail.com>
2025-09-09 13:44:57 -0400

    Update R/MTLR_pred_model_f.R to improve performance

--- R/MTLR_pred_model_f.R
+++ R/MTLR_pred_model_f.R
@@ -108,9 +108,25 @@ MTLR_pred_model_f <- function(train_clin_data, test_clin_data, Model_type,
     sel_clin_tr2 <- na.omit(sel_clin_tr1)
     # create MTLR  model
     formula1 <- survival::Surv(OS_month, OS) ~ .
-    # make  model!
+     
+    # Cross-validation to select best C1
+    cv_result <- MTLR::mtlr_cv(
+    formula = formula1,          # survival formula
+    data = sel_clin_tr2,         # training data
+    C1_vec = c(0.001, 0.01, 0.1, 1, 10, 100, 1000), # candidate C1 values
+    nfolds = 5,                  # number of CV folds
+    foldtype = "fullstrat",      # can also use "censorstrat" or "random"
+    loss = "ll",                 # can also use "concordance"
+    verbose = F)
+
+    # Best C1
+    best_C1 <- cv_result$best_C1
+    
+    # make  model
     Mod1 <- MTLR::mtlr(formula = formula1, data = sel_clin_tr2)
     
+    # Fit final MTLR model using selected C1
+    Mod1 <- MTLR::mtlr(formula = formula1, data = sel_clin_tr2, C1 = best_C1) 
     # Predictions on training data
     survival_curves_tr <- predict(Mod1, sel_clin_tr2, type = "survivalcurve")
     mean_survival_tr <- predict(Mod1, sel_clin_tr2, type = "mean_time")
