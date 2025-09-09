c4cde71f519ba832f316ea69b24236172837d993
Harpreet Kaur <hks04180@gmail.com>
2025-09-09 13:59:45 -0400

    Update R/MTLR_pred_model_f.R to  fix issues

--- R/MTLR_pred_model_f.R
+++ R/MTLR_pred_model_f.R
@@ -116,7 +116,7 @@ MTLR_pred_model_f <- function(train_clin_data, test_clin_data, Model_type,
     nintervals = 10,
     #C1_vec = c(0.001, 0.01, 0.1, 1, 10, 100, 1000), # candidate C1 values
     C1_vec = c(0.01, 0.1, 1)
-    seed_weights = NULL,   # avoid mismatch
+    #seed_weights = NULL,   # avoid mismatch
     nfolds = 5,                  # number of CV folds
     foldtype = "fullstrat",      # can also use "censorstrat" or "random"
     loss = "ll",                 # can also use "concordance"
